a32dce45dfd32e08c78d7f7310069944b836d91d
YuYoHan <zxzz8014@naver.com>
2025-05-02 22:08:53 +0900

    [Study] step8 : RetryTemplate 방법으로 수정

--- src/main/java/org/example/rabbitmq/step7/RabbitMQConfig.java
+++ src/main/java/org/example/rabbitmq/step7/RabbitMQConfig.java
@@ -1,50 +1,50 @@
-package org.example.rabbitmq.step7;
-
-import org.springframework.amqp.core.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQConfig {
-    public static  final String ORDER_COMPLETED_QUEUE = "order_completed_queue";
-    public static  final String ORDER_COMPLETED_EXCHANGE = "order_completed_exchange";
-    public static final  String DLQ = "deadLetterQueue";
-    public static final String DLX = "deadLetterExchange";
-
-    @Bean
-    public TopicExchange orderExchange() {
-        return new TopicExchange(ORDER_COMPLETED_EXCHANGE);
-    }
-
-    @Bean
-    public TopicExchange deadLetterExchange() {
-        return new TopicExchange(DLX);
-    }
-
-    // 메시지가 처리되지 못할 경우 자동으로 DeadLetterQueue에 이동시킴
-    @Bean
-    public Queue orderQueue() {
-        return QueueBuilder.durable(ORDER_COMPLETED_QUEUE)
-                .withArgument("x-dead-letter-exchange", DLX)
-                .withArgument("x-dead-letter-routing-key", DLQ)
-                .ttl(5000)
-                .build();
-    }
-
-    @Bean
-    public Queue deadLetterQueue() {
-        return new Queue(DLQ);
-    }
-
-    @Bean
-    public Binding orderCompletedBinding() {
-        return BindingBuilder.bind(orderQueue()).to(orderExchange()).with("order_completed.#");
-    }
-
-    @Bean
-    public Binding deadLetterBinding() {
-        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with(DLQ);
-    }
-}
-
-
+//package org.example.rabbitmq.step7;
+//
+//import org.springframework.amqp.core.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQConfig {
+//    public static  final String ORDER_COMPLETED_QUEUE = "order_completed_queue";
+//    public static  final String ORDER_COMPLETED_EXCHANGE = "order_completed_exchange";
+//    public static final  String DLQ = "deadLetterQueue";
+//    public static final String DLX = "deadLetterExchange";
+//
+//    @Bean
+//    public TopicExchange orderExchange() {
+//        return new TopicExchange(ORDER_COMPLETED_EXCHANGE);
+//    }
+//
+//    @Bean
+//    public TopicExchange deadLetterExchange() {
+//        return new TopicExchange(DLX);
+//    }
+//
+//    // 메시지가 처리되지 못할 경우 자동으로 DeadLetterQueue에 이동시킴
+//    @Bean
+//    public Queue orderQueue() {
+//        return QueueBuilder.durable(ORDER_COMPLETED_QUEUE)
+//                .withArgument("x-dead-letter-exchange", DLX)
+//                .withArgument("x-dead-letter-routing-key", DLQ)
+//                .ttl(5000)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Queue deadLetterQueue() {
+//        return new Queue(DLQ);
+//    }
+//
+//    @Bean
+//    public Binding orderCompletedBinding() {
+//        return BindingBuilder.bind(orderQueue()).to(orderExchange()).with("order_completed.#");
+//    }
+//
+//    @Bean
+//    public Binding deadLetterBinding() {
+//        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with(DLQ);
+//    }
+//}
+//
+//
---
+++ src/main/java/org/example/rabbitmq/step8/OrderConsumer.java
@@ -0,0 +1,40 @@
+package org.example.rabbitmq.step8;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderConsumer {
+    private final RabbitTemplate rabbitTemplate;
+    private final RetryTemplate retryTemplate;
+
+    @RabbitListener(queues = RabbitMQConfig.ORDER_COMPLETED_QUEUE)
+    public void consume(String message) {
+        retryTemplate.execute(context -> {
+            try {
+                log.debug("리시브 메시지 : " + message + " # retry : " + context.getRetryCount());
+
+                // 실패 조건
+                if("fail".equalsIgnoreCase(message)) {
+                    throw new RuntimeException(message);
+                }
+                log.debug("# 메시지 처리 성공 : " + message);
+            } catch (Exception e) {
+                if(context.getRetryCount() >= 2) {
+                    rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_TOPIC_DLX,
+                            RabbitMQConfig.DEAD_LETTER_ROUTING_KEY, message);
+                } else {
+                    throw e;
+                }
+            }
+            return null;
+        });
+    }
+}
